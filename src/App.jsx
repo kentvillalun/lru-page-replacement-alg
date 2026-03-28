b12fbc7aef745fa3aaf093688965942f1919f67a
KentDoCode <villalunkent03@gmail.com>
2026-03-28 15:53:38 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <main className="min-h-screen bg-[#0a0a0f]">
+
+      </main>
+    </>
+      
+  )
+}
+
+export default App
